08d818f6437b69a3b81d5e5d50411b6b8cba1cb6
1986prasenjit <prasenjitbasu44@gmail.com>
2025-07-30 17:04:28 +0530

    made few changes in Day-1

--- Day-1/script.js
+++ Day-1/script.js
@@ -25,6 +25,35 @@ const address2 = "USA";// Here we will get an error because const cannot be rede
 console.log(address);
 */
 
+/*
+- **Primitive Data Types:**
+    - `String` - Text values ("Hello")
+    - `Number` - Numeric values (25, 3.14)
+    - `Boolean` - True/False (true, false)
+    - `Undefined` - A variable declared but not assigned (let x;)
+    - `Null` - Represents "nothing" (let y = null;)
+    - `BigInt` - Large numbers (BigInt(12345678901234567890))
+    - `Symbol` - Unique identifiers (Symbol("id"))
+- **Non-Primitive (Reference) Data Types:**
+    - `Object` - Collection of key-value pairs
+    - `Array` - Ordered list of values
+    - `Function` - Code that can be executed
+  */
+
+let student = {
+    name: "Alice",
+    age: 22,
+    isEnrolled: true
+};
+console.log(student.name); // Output: Alice   
+
+let arr = [1, 2, 4, 5]
+
+
+let a = 10;
+let b = a; // 10
+b = 20; // 20
+console.log(a); // 10 (original remains unchanged)
 
 
 
