ffdd05ce4fbdf5c1fe3741964756250a61281752
1986prasenjit <prasenjitbasu44@gmail.com>
2025-07-30 16:45:28 +0530

    added Day-1 Of JS explanation of variables and Data types

---
+++ Day-1/script.js
@@ -0,0 +1,30 @@
+console.log("Hello World");
+
+var name1;
+let name2;
+//!const name3;// We Cannot do this when a variable is declared with const, the error we will get is:- SyntaxError: Missing initializer in const declaration;
+
+//Variables are used to store data in JS
+/* 
+//`var`: Function-scoped, can be redeclared (not recommended);
+var address = "Bangalore";
+console.log(address);
+var address = "USA";//Valid for var
+console.log(address);
+
+//`let`: Block-scope, can be reassigned
+let address1 = "Bangalore";
+console.log(address);
+let address1 = "USA";// Here we will get an error because let cannot be redeclared, but let can be reassigned. Error we will get :- Identifier 'address1' has already been declared
+console.log(address);
+
+//`const`: Block-scope, **cannot** be reassigned.
+const address2 = "Bangalore";
+console.log(address);
+const address2 = "USA";// Here we will get an error because const cannot be redeclared nor cannot be reassigned.
+console.log(address);
+*/
+
+
+
+
